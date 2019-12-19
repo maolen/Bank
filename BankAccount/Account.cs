2633127a703f8eb00dac808d9ec3c6533c3c43dc
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-19 21:11:26 +0600

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class Account
+    {
+
+        private object syncObject = new object();
+        public int Balance { get; set; } = 10000;
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public void ChangeBalance(object moneySum)
+        {
+            Balance += (int) moneySum;
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Linq;
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ACCOUNT_COUNT = 100;
+            const int THREAD_COUNT = 1000;
+            var accounts = new BlockingCollection<Account>();
+
+            for (var i = 0; i < ACCOUNT_COUNT; i++)
+            {
+                var account = new Account();
+                accounts.Add(account);
+            }
+
+            for (var i = 0; i < THREAD_COUNT; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    var random = new Random();
+                    var index = random.Next(0, accounts.Count);
+                    var account = accounts.ElementAt(index);
+                    lock (account)
+                    {
+                        ThreadPool.QueueUserWorkItem(account.ChangeBalance, 100);
+                        Console.WriteLine($"Счёт пополнен на 100, баланс равен {accounts.ElementAt(index).Balance}");
+                    }
+                }
+                else
+                {
+                    var random = new Random();
+                    var index = random.Next(0, accounts.Count);
+                    var account = accounts.ElementAt(index);
+                    lock (account)
+                    {
+                        ThreadPool.QueueUserWorkItem(account.ChangeBalance, -100);
+                        Console.WriteLine($"Со счёта снято 100, баланс равен {account.Balance}");
+                    }
+                }
+
+            }
+            foreach (var account in accounts)
+            {
+                Console.WriteLine(account.Id + " - " + account.Balance);
+            }
+        }
+    }
+}
